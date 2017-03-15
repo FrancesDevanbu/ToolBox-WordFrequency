0d6501dc886996d5a3ab9a28acdbbfed3cc23800
FrancesDevanbu <frances.devanbu@students.olin.edu>
2017-03-14 22:31:18 -0400

    word frequency toolbox

--- frequency.py
+++ frequency.py
@@ -10,8 +10,24 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    curr_line = 0
+    while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
+        curr_line += 1
+    lines = lines[curr_line+1:]
+    working_set = ''
 
+    for line in lines:
+        if not line =='\n':
+            working_set = working_set + line
+    working_set= working_set.split()
+    wordnum = 0
+    for word in working_set:
+        working_set[wordnum] = word.strip(string.punctuation).lower()
+        wordnum +=1
+
+    return working_set
 
 def get_top_n_words(word_list, n):
     """ Takes a list of words as input and returns a list of the n most frequently
@@ -23,8 +39,19 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    word_count = dict()
+    for word in word_list:
+        word_count[word] = word_count.get(word,0)+1
+
+    ordered_by_frequency = sorted(word_count, key=word_count.get, reverse=True)
+
+    return ordered_by_frequency[:n]
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    working_set = get_word_list('alice.txt')
+    top_list = get_top_n_words(working_set, 100)
+
+    #print("Running WordFrequency Toolbox")
+    #print(string.punctuation)
+    print(top_list)
